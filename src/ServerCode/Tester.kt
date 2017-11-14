321e05c2c188f84ecce03e5d7e64b7ccd294529a
Marie Payne <marie.payne@hotmail.ca>
2017-11-13 22:09:33 -0500

    Update Tester.kt
    performance analysis tester code

--- src/ServerCode/Tester.kt
+++ src/ServerCode/Tester.kt
@@ -8,6 +8,7 @@ import Transactions.TransactionalMiddleware
 import Transactions.TransactionalRequestReceiver
 import Transactions.TransactionalRequestSender
 import kotlin.concurrent.thread
+import kotlin.system.measureTimeMillis
 
 
 object Tester {
@@ -17,14 +18,9 @@ object Tester {
     @JvmStatic
     fun main(args: Array<String>) {
 
-        //testBasics()
-        //testMiddleware()
-        //testLockManager()
-        testTransactions()
+        println(" Performance Analysis Testing: \n")
+        println("START: \n")
 
-    }
-
-    fun testTransactions() {
         val requestReceiverFlight = TcpRequestReceiver(ResourceManagerImpl(), PortNumbers.flightRm)
         requestReceiverFlight.runServer()
 
@@ -39,193 +35,114 @@ object Tester {
 
         Thread.sleep(500)
 
-        val middleware = TransactionalRequestReceiver(TransactionalMiddleware("127.0.0.1"), PortNumbers.middleware)
-        middleware.runServer()
+        val midware = TransactionalRequestReceiver(TransactionalMiddleware("127.0.0.1"), PortNumbers.middleware)
+        midware.runServer()
 
         Thread.sleep(500)
 
         val client = TransactionalRequestSender(PortNumbers.middleware, "127.0.0.1")
 
-        client.createResource(1, ReservableType.FLIGHT, "fly_1", 50, 500)
+        // Initiate resources to test on
+        if (client.start(12) == true) {
+            client.createResource(12, ReservableType.FLIGHT, "fly_1", 50, 5)
+            client.createResource(12, ReservableType.HOTEL, "hot_1", 50, 5)
+            client.createResource(12, ReservableType.CAR, "car_1", 50, 5)
+        }
+        if (client.commit(12) == false){
+            println("Your transaction failed.\n")
+        }
 
-        client.start(3)
+        var clients = 1
 
-        client.createResource(3, ReservableType.FLIGHT, "fly_1", 50, 500)
+        if (clients == 1){
+            customer(client, 1)
+        } else {
 
+            Thread {
+                customer(client, 2)
+            }.start()
+            Thread {
+                customer(client, 3)
+            }.start()
+            Thread {
+                customer(client, 4)
+            }.start()
+        }
 
-
-        val resource = client.queryResource(3, "fly_1")
-        println("\n\n\nresource 1 $resource")
-
-        client.abort(3)
-
-
-        println("\n\n\n")
-        val resource2 = client.queryResource(3, "fly_1")
-        println("\n\n\nresource 2 $resource2")
-
-        println("\n\n DONE")
-
+        println("END\n")
 
     }
 
-    fun testLockManager() {
-        val lm = LockManager()
 
-        val r9 = lm.lock(3, "a", LockType.READ)
-        println("3 acquired A_read: $r9")
+    fun customer(middleware: TransactionalRequestSender, custID: Int){
+        // The customer transaction types are: add reservation, itinerary
+        // We assume all other transaction types are administrative, e.g. not accessible to
+        // the average customer.
 
-        thread {
-            val r1 = lm.lock(1, "a", LockType.WRITE)
-            println("1 acquired A_write: $r1")
+        val resourceIds = arrayOf("car_1", "fly_1", "hot_1")
+        var r1 = 0
+        var r2 = 0
+        var r3 = 0
+        var counter = 1
+        var response2 = false
+        var response3 = false
+        if(middleware.start(999)) {
+            middleware.createCustomer(999, custID)
+            if(!middleware.commit(999)){
+                println("Create customer commit failed. \n")
+            }
+        } else {
+            println("Create customer transaction failed. \n")
         }
 
-        val r10 = lm.unlockAll(3);
-        println("3 unlocked all: $r10")
+        for (i in 0..5) {
+            // Random variables for the resource type, sleep time, and randomized method, respectively.
+            r1 = (Math.random()*3).toInt()
+            r2 = (Math.random()*400).toInt() + 300
+            r3 = (Math.random()*2).toInt() + 1
 
-        thread {
-            val r2 = lm.lock(2, "a", LockType.WRITE)
-            println("2 acquired A_write: $r2")
+            var timeElapsed = measureTimeMillis {
+                if (r3 == 1) {
+                    if(middleware.start(counter)) {
+                        var response = middleware.queryResource(counter, resourceIds[r1])
+                        println("Iteration $i query response: $response")
+
+                        if (response != null) {
+                            response2 = middleware.customerAddReservation(counter, custID, counter, response.item)
+                            middleware.reserveResource(counter, resourceIds[r1], 1)
+                            println("Iteration $i reserve response: $response2")
+                        }
+                        if(!middleware.commit(counter)){
+                            println("Iteration $i commit failed. \n")
+                        }
+                    } else {
+                        println("Iteration $i transaction failed. \n")
+                    }
+                } else if (r3 == 2) {
+                    if(middleware.start(counter)) {
+                        var resource1 = middleware.queryResource(counter, resourceIds[r1])
+                        r1 = (Math.random() * 3).toInt()
+                        var resource2 = middleware.queryResource(counter, resourceIds[r1])
+                        if (resource1 != null && resource2 != null) {
+                            response3 = middleware.itinerary(counter, custID, mutableMapOf(1 to resource1.item, 2 to resource2.item))
+                        }
+                        println("Iteration $i itinerary response: $response3")
+                        if(!middleware.commit(counter)){
+                            println("Iteration $i commit failed. \n")
+                        }
+                    } else {
+                        println("Iteration $i transaction failed. \n")
+                    }
+                }
+            }
+            println("Time Elapsed: $timeElapsed \n")
+            counter++
+            if (custID > 1) {
+                Thread.sleep(r2.toLong())
+            }
         }
-
-        val r4 = lm.lock(1, "a", LockType.READ)
-        println("1 acquired A_read: $r4")
-
-        val r5 = lm.lock(1, "a", LockType.WRITE)
-        println("1 acquired A_write: $r5")
-
-        Thread.sleep(3000)
-        val r3 = lm.unlockAll(1)
-        println("1 unlocked all: $r3")
-
 
     }
 
-    fun testMiddleware() {
-
-        val requestReceiverFlight = TcpRequestReceiver(ResourceManagerImpl(), PortNumbers.flightRm)
-        requestReceiverFlight.runServer()
-
-
-        val requestReceiverHotel = TcpRequestReceiver(ResourceManagerImpl(), PortNumbers.hotelRm)
-        requestReceiverHotel.runServer()
-
-
-        val requestReceiverCar = TcpRequestReceiver(ResourceManagerImpl(), PortNumbers.carRm)
-        requestReceiverCar.runServer()
-
-        Thread.sleep(500)
-
-
-        val middleware = TcpRequestReceiver(Middleware("127.0.0.1", 0), PortNumbers.middleware)
-        middleware.runServer()
-
-        val client = TcpRequestSender(PortNumbers.middleware, "127.0.0.1", 0)
-
-
-
-        println("\n\n\n TEST 1")
-        val response = client.createResource(ReservableType.FLIGHT, "fly_1", 5, 5)
-        println("TESTER received response: $response")
-
-
-
-
-        println("\n\n\n TEST 2")
-        val response2 = client.queryResource("fly_1")
-        val resource1 = response2
-        println("TESTER query previous resource: $response2")
-
-
-
-        println("\n\n\n TEST 3")
-        val response3 = client.queryResource("hotel_5")
-        println("TESTER query nonexistant resource: $response3")
-
-
-
-        println("\n\n\n TEST 4")
-        val response4 = client.createCustomer(5)
-        println("TESTER created customer: $response4")
-
-
-
-        println("\n\n\n TEST 5")
-        var response5: Boolean = false
-        if (resource1 != null) {
-            response5 = client.customerAddReservation(5, 12, resource1.item)
-        }
-        println("TESTER response is $response5")
-
-
-
-        println("\n\n\n TEST 6")
-        val response6 = client.queryCustomer(5)?.reservations
-        println("TESTER customer now has $response6.")
-
-
-
-        println("\n\n\n TEST 7 itinerary")
-        client.createResource(ReservableType.CAR, "car_1", 10, 1)
-
-        val resource2 = client.queryResource("car_1")
-        if (resource1 != null && resource2 != null) {
-            client.itinerary(5, mutableMapOf(1 to resource1.item, 2 to resource2.item))
-        }
-        val newReservations = client.queryCustomer(5)?.reservations
-        println("TESTER customer now has $newReservations.")
-
-    }
-
-    fun testBasics() {
-        println("try creating customer")
-        tester.createCustomer(tester.uniqueCustomerId())
-        pp()
-
-        println("try creating two customers with same id")
-        println(tester.createCustomer(1))
-        pp()
-
-        println("try creating second customer")
-        tester.createCustomer(tester.uniqueCustomerId())
-        pp()
-
-        println("try deleting valid customer 1")
-        tester.deleteCustomer(1)
-        pp()
-
-        println("try deleting invalid customer 5 ")
-        tester.deleteCustomer(5)
-        pp()
-
-        println("create flight 5")
-        tester.createResource(ReservableType.FLIGHT, "5", 5, 500)
-        pp()
-
-        println("create car 10")
-        tester.createResource(ReservableType.CAR, "10", 10, 50)
-        pp()
-
-        println("create hotel 10")
-        tester.createResource(ReservableType.HOTEL, "10", 100, 300)
-        pp()
-
-        println("update flight 20, 10000")
-        tester.updateResource("5", 20, 10000)
-        pp()
-
-        println("delete resource")
-        tester.deleteResource("5")
-        pp()
-    }
-
-    fun pp() {
-        for (customer in tester.customers) {
-            println("$customer")
-        }
-        for (resource in tester.resources) {
-            println("$resource")
-        }
-        println()
-    }
 }
+
